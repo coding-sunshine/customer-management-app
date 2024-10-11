7d28cc17e75b696948bec9c1e4152cef0163e6c3
Hardik Shah <sunshine.mahant@gmail.com>
2024-10-11 12:05:46 +1100

    common http request added with axios

---
+++ resources/js/requests/index.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+const http = axios.create({
+    baseURL: import.meta.env.VITE_BASE_API_URL,
+    headers:{
+        accept: 'application/json'
+        // 'Access-Control-Allow-Origin': 'tb10'
+    }
+});
+http.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        if (error.response ) {
+            router.push({name: 'error'});
+        }
+        return Promise.reject(error);
+    }
+);
+return http;
