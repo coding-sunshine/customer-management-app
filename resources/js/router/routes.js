2838e58277592c1b2922df1eb7a6a815e6e333ec
Hardik Shah <sunshine.mahant@gmail.com>
2024-10-11 12:00:29 +1100

    Blank customer view Added

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -1,3 +1,16 @@
+import CustomerList from '../components/customer/CustomerList.vue';
 const routes = [
+    {
+        path: '/app/',
+        redirect:'/app/customers'
+    },
+    {
+        path: '/app/customers',
+        name: 'customers',
+        component: CustomerList,
+        meta:{
+            title: 'Customers'
+        }
+    },
 ];
 export default routes;
