f9acfd034426684b2161b44176c4b2f39e23b345
Hardik Shah <sunshine.mahant@gmail.com>
2024-10-11 11:55:49 +1100

    Added Vue Router and configured

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,11 @@
 import './bootstrap';
 
 import { createApp } from 'vue';
+import router from './router';
+import 'flowbite';
+
 import App from './components/App.vue';
 
 const app = createApp(App);
+app.use( router );
 app.mount('#app');
-
-import 'flowbite';
